b623e36e12feb0b320d5ce0fe8358b2ac4942df6
maximilianofried <maximilianofried@gmail.com>
2020-06-18 13:53:45 +0200

    database config

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -4,10 +4,15 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: env('DATABASE_URI'),
+        host: env('DATABASE_HOST','127.0.0.1'),
+        srv: env.bool('DATABASE_SRV', false),
+        port: env.int('DATABASE_PORT',27017),
+        database: env('DATABASE_NAME','gt-strapi-db'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
       },
       options: {
-        ssl: true,
+        ssl: env.bool('DATABASE_SSL', false),
       },
     },
   },

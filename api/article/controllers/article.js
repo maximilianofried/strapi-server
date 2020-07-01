b5f4e6b4e10c7ed73da5166ad5223fe191b5b3e4
maximilianofried <maximilianofried@gmail.com>
2020-07-01 18:02:26 +0200

    adds comment system in article controller

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -1,6 +1,7 @@
 const { sanitizeEntity } = require('strapi-utils');
 
 module.exports = {
+
   async find(ctx) {
     let entities;
 
@@ -15,5 +16,18 @@ module.exports = {
     }
 
     return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.article }));
+  },
+
+  async comment(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.comment.create(data, { files });
+    } else {
+      //ctx.request.body.author = ;
+      ctx.request.body.article = ctx.params.id;
+      entity = await strapi.services.comment.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.comment });
   }
-};
+}; 
